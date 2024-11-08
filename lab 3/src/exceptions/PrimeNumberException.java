dcf05cbd54fa2dfa7bc47e712a6f68a7226f5607
Iurii Bogdanov <145650269+iurii1801@users.noreply.github.com>
2024-11-08 15:27:38 +0200

    Delete lab 3 directory

--- lab 3/src/exceptions/PrimeNumberException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class PrimeNumberException extends Exception {
-  public PrimeNumberException(int invalidNumber) {
-    super("Ошибка: Число " + invalidNumber + " не является простым.");
-  }
-}
